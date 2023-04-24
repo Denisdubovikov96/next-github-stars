668479a3c26537512d418c21440ac754e1f4ea12
Denis Dubovikov <denis@chalklinesports.com>
2023-04-24 15:04:04 +0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import '#commponents/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
